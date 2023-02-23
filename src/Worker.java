e9e960e337af3c726fd808d514777b749369c7a1
Pablo Nguyen <pnguy150@calpoly.edu>
2023-02-22 19:13:10 -0800

    create worker, added some code to World

---
+++ src/Worker.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+
+public interface Worker {
+    void draw(Graphics g);
+}
--- src/World.java
+++ src/World.java
@@ -1,14 +1,22 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class World extends JFrame implements ActionListener {
+    private Source source;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        World world = new World();
+        world.setSize(500,500);
+        world.setVisible(true);
+        world.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        source.create();
         
     }
 }
