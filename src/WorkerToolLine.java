4dcd025f455d20d29dbef8db528d26bfa8c50ca2
Pablo Nguyen <pnguy150@calpoly.edu>
2023-02-23 13:39:58 -0800

    Create WorkerStandard and WorkerTool

---
+++ src/WorkerToolLine.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.List;
+
+public class WorkerToolLine extends WorkerTool{
+    private static final int BAR_WIDTH = 10;
+    public final int MAX_NUMBER_OF_VALUES = 20;
+
+    public void draw(Graphics g, List values){
+        g.setColor(Color.gray);
+        for (int i = 0; i <values.size(); i++){
+            g.fillRect(
+                    (500/ MAX_NUMBER_OF_VALUES *i )  - (BAR_WIDTH/2),
+                    (int)values.get(i),
+                    BAR_WIDTH,
+                    200 - (int) values.get(i)
+            );
+        }
+        super.draw(g,values);
+    }
+}
